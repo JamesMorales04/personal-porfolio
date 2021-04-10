84cf436aa1a48970219e505d5d1f6baea3370917
James Morales <jamesmoralesmoreno@gmail.com>
2021-04-09 21:46:07 -0500

    Command formated

--- uploadChanges.sh
+++ uploadChanges.sh
@@ -1,39 +1,35 @@
 #!/bin/bash
 
-
-for arg in "$@"
-do
+for arg in "$@"; do
     echo "$arg"
 done
 
-if [[ $1 != "" && $2 != "" && $3 != "" ]];
+if [[ $1 != "" && $2 != "" && $3 != "" ]]; then
 
-	then
+    #Client commit
+    cd personalPortfolioClient || exit
+    git add .
+    git commit -m "$1"
+    git push
+    cd ..
 
-        #Client commit
-	    cd personalPortfolioClient || exit
-        git add .
-        git commit -m "$1"
-        git push
-        cd ..
+    #Server commit
+    cd personalPortfolioServer || exit
+    git add .
+    git commit -m "$2"
+    git push
+    cd ..
 
-        #Server commit
-        cd personalPortfolioServer || exit
-        git add .
-        git commit -m "$2"
-        git push
-        cd ..
+    #Big repo commit
+    git add .
+    git commit -m "$3"
+    git push
 
-        #Big repo commit
-        git add .
-        git commit -m "$3"
-        git push
+else
 
-    else
+    echo -e "3 parameters need to be entered:"
+    echo -e "1: Commit message for the client"
+    echo -e "2: Commit message for the server"
+    echo -e "3: Commit message for the big repo"
 
-	  echo -e "3 parameters need to be entered:"
-      echo -e "1: Commit message for the client"
-      echo -e "2: Commit message for the server"
-      echo -e "3: Commit message for the big repo"
-
-	fi
+fi
