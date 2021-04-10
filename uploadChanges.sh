9dcbf53fb087e8e07aecc12335ad38dd2765ceb5
James Morales <jamesmoralesmoreno@gmail.com>
2021-04-09 22:06:30 -0500

    Changes in upload command

--- uploadChanges.sh
+++ uploadChanges.sh
@@ -1,35 +1,26 @@
 #!/bin/bash
 
-for arg in "$@"; do
-    echo "$arg"
-done
+function verifyStatus {
+    CHANGED=$(git status --porcelain --untracked-files=no)
 
-if [[ $1 != "" && $2 != "" && $3 != "" ]]; then
+    if [ -n "${CHANGED}" ]; then
+        git add .
+        read -p "Commit description for $1: " description 
+        git commit -m "$description"
+        git push
+        cd ..
+    else
+        cd ..
+    fi
+    
+}
+ls 
+cd personalPortfolioClient || exit
+verifyStatus "Client"
 
-    #Client commit
-    cd personalPortfolioClient || exit
-    git add .
-    git commit -m "$1"
-    git push
-    cd ..
+#Server commit
+cd personalPortfolioServer || exit
+verifyStatus "Server"
 
-    #Server commit
-    cd personalPortfolioServer || exit
-    git add .
-    git commit -m "$2"
-    git push
-    cd ..
-
-    #Big repo commit
-    git add .
-    git commit -m "$3"
-    git push
-
-else
-
-    echo -e "3 parameters need to be entered:"
-    echo -e "1: Commit message for the client"
-    echo -e "2: Commit message for the server"
-    echo -e "3: Commit message for the big repo"
-
-fi
+#Big repo commit
+verifyStatus "General"
