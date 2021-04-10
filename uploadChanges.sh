0dd2d20ea6e16cb69053eb15ca9a221eb6672463
James Morales <jamesmoralesmoreno@gmail.com>
2021-04-09 21:40:00 -0500

    General upload changes command

--- uploadChanges.sh
+++ uploadChanges.sh
@@ -1,34 +1,43 @@
 #!/bin/bash
-echo "First arg: $1"
-echo "Second arg: $2"
-echo "Third arg: $3"
+
+
+arg=0
+
+for arg in "$@"
+do
+    #"$arg"= "$arg" | tr "-" " "
+    echo "$arg"
+    ++arg 
+done
 
 if [[ $1 != "" && $2 != "" && $3 != "" ]];
 
 	then
 
         #Client commit
-	    cd personalPortfolioClient
+	    cd personalPortfolioClient || exit
         git add .
-        git commit -m ""+$1
+        git commit -m "$1"
         git push
         cd ..
 
         #Server commit
-        cd personalPortfolioServer
+        cd personalPortfolioServer || exit
         git add .
-        git commit -m ""+$2
+        git commit -m "$2"
         git push
         cd ..
 
         #Big repo commit
         git add .
-        git commit -m ""+$3
+        git commit -m "$3"
         git push
 
     else
+
 	  echo -e "3 parameters need to be entered:"
       echo -e "1: Commit message for the client"
       echo -e "2: Commit message for the server"
       echo -e "3: Commit message for the big repo"
+
 	fi
