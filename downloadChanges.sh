15ffdbf71e4353f3a4901b98a61cbc88ad4044ef
James Morales <jamesmoralesmoreno@gmail.com>
2021-04-14 21:07:00 -0500

    Download changes command created

---
+++ downloadChanges.sh
@@ -0,0 +1,13 @@
+#Pull server changes
+cd personalPortfolioServer || exit
+git pull
+cd ..
+
+#Pull Client Changes 
+cd personalPortfolioClient || exit
+git pull
+cd ..
+
+#Pull main changes
+
+git pull
